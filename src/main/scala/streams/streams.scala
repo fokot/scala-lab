05fbc19cc704189708e15811d0bd349b00b2f1c2
fokot <frantisek.kocun@gmail.com>
2021-12-26 23:20:26 +0100

    sink

--- src/main/scala/streams/streams.scala
+++ src/main/scala/streams/streams.scala
@@ -2,7 +2,7 @@ package streams
 
 import zio.*
 
-import java.io.{BufferedReader, FileReader}
+import java.io.{BufferedReader, BufferedWriter, File, FileReader, FileWriter}
 
 /**
  * Streams from amazing Zymposium sessions:
@@ -13,6 +13,11 @@ import java.io.{BufferedReader, FileReader}
  */
 object ZStreamTest extends ZIOAppDefault {
 
+  val simpleSink = ZSink.runCollect[String]
+  val simpleSink2 = ZSink.toFile("sink-zip-test")
+  val notSoSimpleSink = simpleSink.zipWithPar(simpleSink2)((o, _) => o)
+  val simpleStream = ZStream.fromIterator(List(4, 5, 6).iterator)
+
   override def run: ZIO[ZEnv with ZIOAppArgs, Any, Any] =
     for {
       // it is processing elements one by one
@@ -22,10 +27,13 @@ object ZStreamTest extends ZIOAppDefault {
         .map(_ + 100)
         .tap(a => ZIO.debug(s"BIGGER $a"))
         .take(3)
+        .map(_.toString)
         .runCollect
       _ <- ZIO.debug(res1)
 
-      _ <- ZStream.lines("build.sbt").tap(a => ZIO.debug(a).delay(500.milliseconds)).runDrain
+      //      _ <- ZStream.lines("build.sbt").tap(a => ZIO.debug(a).delay(500.milliseconds)).runDrain
+
+      _ <- simpleStream.map(_.toString).run(notSoSimpleSink).debug("To Sink")
     } yield ExitCode.success
 }
 
@@ -38,7 +46,7 @@ object Pull {
 // A internally will be Chunk[A] for performance reasons
 case class ZStream[-R, +E, +A](
   process: ZManaged[R, E, ZIO[R, Option[E], A]]
-) {
+) { self =>
 
   def tap[R1 <: R](f: A => URIO[R1, Any]): ZStream[R1, E, A] =
     ZStream(
@@ -62,6 +70,25 @@ case class ZStream[-R, +E, +A](
       )
     ))
 
+  def run[R1 <: R, E1 >: E, O](sink: ZSink[R1, E1, A, O]): ZIO[R1, E1, O] =
+    process.zip(sink.run).use { case (pull, push) =>
+      def loop: ZIO[R1, E1, O] =
+        pull.foldZIO(
+          {
+            case Some(e) => ZIO.fail(e)
+            case None => push(Chunk.empty).flatMap {
+              case Some(o) => ZIO.succeed(o)
+              case None => ZIO.dieMessage("Sink violated contract by returning None after being pushed empty Chunk")
+            }
+          },
+          a => push(Chunk.single(a)).flatMap {
+            case Some(o) => ZIO.succeed(o)
+            case None => loop
+          }
+        )
+
+      loop
+    }
 
   def runCollect: ZIO[R, E, Chunk[A]] =
     process.use { pull =>
@@ -89,6 +116,29 @@ case class ZStream[-R, +E, +A](
         )
       loop
     }
+
+  //FIXME
+//  def runToFile(file: String)(implicit ev: A <:< String, ev2: E <:< Throwable): ZIO[R, Throwable, Unit] =
+//  def runToFile(file: String)(implicit ev: A =:= String): ZIO[R, Throwable, Unit] =
+//    self.run(ZSink.toFile(file))
+
+  //  def runToFile(file: String)(implicit ev: A <:< String): ZIO[R, E, Unit] =
+  //    process.use { pull =>
+  //      ZIO.succeed(new File(file)).flatMap(
+  //        file => ZIO.succeed(new BufferedWriter(new FileWriter(file))).flatMap {
+  //          writer =>
+  //            lazy val loop: ZIO[R, E, Unit] =
+  //              pull.foldZIO(
+  //                {
+  //                  case None => ZIO.unit
+  //                  case Some(err) => ZIO.fail(err)
+  //                },
+  //                a => ZIO.succeed(writer.write(a)) *> loop
+  //              )
+  //            loop.ensuring(ZIO.succeed(writer.close()))
+  //        }
+  //      )
+  //    }
 }
 
 object ZStream {
@@ -116,5 +166,68 @@ object ZStream {
     )
 }
 
+// PROTOCOL
+// Empty Chunk of input means no more input
+// Succeed with Some(O) means "I'm done with summary value O"
+// Succeed with None means "Feed me more input"
+// Fail with E means "I'm done with an error E"
+// "Leftovers" can be added
+case class ZSink[-R, +E, -I, +O](
+  run: ZManaged[R, E, Chunk[I] => ZIO[R, E, Option[O]]]
+) { self =>
 
+  def zipWithPar[R1 <: R, E1 >: E, I1 <: I, O2, O3](
+    that: ZSink[R1, E1, I1, O2]
+  )(f: (O, O2) => O3): ZSink[R1, E1, I1, O3] =
+    ZSink(
+      self.run.zipPar(that.run).zipPar(Ref.make[Option[Either[O, O2]]](None).toManaged).map {
+        case (pushLeft, pushRight, ref) =>
+          in =>
+            ref.get.flatMap {
+              case None =>
+                pushLeft(in).zipPar(pushRight(in)).flatMap {
+                  case (Some(o), Some(o2)) => ZIO.some(f(o, o2))
+                  case (Some(o), None)     => ref.set(Some(Left(o))).as(None)
+                  case (None, Some(o2))    => ref.set(Some(Right(o2))).as(None)
+                  case (None, None)        => ZIO.none
+                }
+              case Some(Left(o)) =>
+                pushRight(in).map {
+                  case Some(o2) => Some(f(o, o2))
+                  case None     => None
+                }
+              case Some(Right(o2)) =>
+                pushLeft(in).map {
+                  case Some(o) => Some(f(o, o2))
+                  case None     => None
+                }
+            }
+      }
+    )
+}
+
+object ZSink {
+
+  def runCollect[A]: ZSink[Any, Nothing, A, Chunk[A]] =
+    ZSink(
+      Ref.make[Chunk[A]](Chunk.empty).toManaged.map(ref =>
+        in =>
+          // Decided for contract that empty chunk means end of stream
+          if (in.isEmpty) ref.get.asSome
+          else ref.update(_ ++ in).as(None)
+      )
+    )
+
+  def writer(file: String) =
+    ZManaged.acquireReleaseWith(
+      ZIO.attempt(new BufferedWriter(new FileWriter(file)))
+    )(writer => ZIO.attempt(writer.close()).ignore)
+
+  def toFile(file: String): ZSink[Any, Throwable, String, Unit] =
+    ZSink(writer(file).map { writer =>
+      in =>
+        if (in.isEmpty) ZIO.some(())
+        else ZIO.foreachDiscard(in)(s => ZIO.succeed(writer.write(s))).as(None)
+    })
+}
 
